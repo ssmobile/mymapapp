6c57af14896b21ae2528cd201e520feb45849136
ssmobile <spyridon.magliveras@mobileappscompany.us>
2019-09-24 14:09:58 -0400

    Add custom location marker

--- app/src/main/java/com/example/mymapapp/MapsActivity.kt
+++ app/src/main/java/com/example/mymapapp/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mymapapp
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
@@ -14,9 +16,23 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.core.graphics.drawable.DrawableCompat
+import android.os.Build
+import androidx.core.content.ContextCompat
+import android.graphics.drawable.Drawable
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -78,7 +94,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun displayLocationOnMap(latLng: LatLng, title : String) {
-        mMap.addMarker(MarkerOptions().position(latLng).title(title))
+
+        mMap.addMarker(MarkerOptions()
+            .position(latLng)
+            .title(title)
+            .icon(BitmapDescriptorFactory.fromBitmap(
+                getBitmapFromVectorDrawable(this, R.drawable.ic_beenhere_24dp))
+            ))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
     }
 
@@ -86,9 +108,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val usa = LatLng(38.063741, -95.534281)
+        mMap.setMinZoomPreference(3.3f)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(usa))
+    }
+
+    private fun getBitmapFromVectorDrawable(context: Context, drawableId: Int): Bitmap {
+        var drawable = ContextCompat.getDrawable(context, drawableId)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = DrawableCompat.wrap(drawable!!).mutate()
+        }
+
+        val bitmap = Bitmap.createBitmap(
+            drawable!!.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+        drawable.draw(canvas)
+
+        return bitmap
     }
 }
